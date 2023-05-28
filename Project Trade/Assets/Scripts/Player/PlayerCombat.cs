4843a1d1a23c647550ff33b645ecf68b696a7ec5
PursuedFireX <116890485+PursuedFireX@users.noreply.github.com>
2023-05-27 21:51:01 -0400

    New Combo System
    5/27/23

--- Project Trade/Assets/Scripts/Player/PlayerCombat.cs
+++ Project Trade/Assets/Scripts/Player/PlayerCombat.cs
@@ -8,38 +8,98 @@ namespace PFX
     {
         private Animator anim;
         private float cooldown;
-        private float nextTimeToFire = 0;
-        public static int numberOfClicks;
-        private float lastTimeClicked = 0;
         private float maxComboDelay = 1;
         private bool playNext;
         private float animationPlayTime;
+
+        private int clicks;
+        private int combo;
+        private float lastTimeClicked;
+
+        public AnimatorOverrideController[] attacks;
+        
 
         public void Initialize(Animator anim, float cooldown, float playTime)
         {
             this.anim = anim;
             this.cooldown = cooldown;
             this.animationPlayTime = playTime;
-            numberOfClicks = 0;
             playNext = false;
         }
 
         public void CombatHandler(bool hasSword, bool hasTool)
         {
             ResetAnimationBools();
+            AnimationStateUpdate(hasSword, hasTool);
 
-            if(InputManager.I.Fire1())
+
+            if (InputManager.I.Fire1())
             {
                 Fire(hasSword, hasTool);
             }
+            
 
+            if (playNext == true && anim.GetCurrentAnimatorStateInfo(2).normalizedTime > animationPlayTime)
+            {
+                anim.runtimeAnimatorController = attacks[combo];
+                anim.Play("Attack", 2, 0);
+                playNext = false;
+                
+            }
+        }
+
+
+        private void Fire(bool hasSword, bool hasTool)
+        {
+            anim.SetBool("hasSword", hasSword);
+            anim.SetBool("isTool", hasTool);
+            lastTimeClicked = Time.time;
+            Debug.Log(combo);
+            Debug.Log(clicks);
+
+            if(clicks == 0)
+            {
+                anim.runtimeAnimatorController = attacks[0];
+                anim.Play("Attack", 2, 0);
+                clicks++;
+            }
+            else if (clicks > 0 && anim.GetCurrentAnimatorStateInfo(2).normalizedTime > .2f && anim.GetCurrentAnimatorStateInfo(2).IsName("Attack"))
+            {
+                playNext = true;
+                if (combo < attacks.Length)
+                    combo++;
+                else
+                    combo = 0;
+
+                if (clicks < attacks.Length)
+                    clicks++;
+                else
+                    clicks = 0;
+            }
+            
+
+            anim.SetLayerWeight(2, 1);
+        }
+
+        private void ResetAnimationBools()
+        {
+
+            if (Time.time - lastTimeClicked > maxComboDelay)
+            {
+                combo = 0;
+                clicks = 0;
+            }
+        }
+
+        private void AnimationStateUpdate(bool hasSword, bool hasTool)
+        {
             if (hasSword)
             {
                 anim.SetBool("hasSword", true);
                 anim.SetBool("isTool", false);
                 anim.SetLayerWeight(1, 1);
             }
-            else if(hasTool)
+            else if (hasTool)
             {
                 anim.SetBool("hasSword", false);
                 anim.SetBool("isTool", true);
@@ -51,77 +111,7 @@ namespace PFX
                 anim.SetBool("isTool", false);
                 anim.SetLayerWeight(1, 0);
             }
-
-            if (playNext == true && anim.GetCurrentAnimatorStateInfo(2).normalizedTime > animationPlayTime)
-            {
-                anim.SetBool("Attack2", true);
-                anim.SetBool("Attack1", false);
-                playNext = false;
-                numberOfClicks = 0;
-            }
         }
 
-
-        private void Fire(bool hasSword, bool hasTool)
-        {
-            if (hasSword)
-            {
-                anim.SetBool("hasSword", true);
-                anim.SetBool("isTool", false);
-                lastTimeClicked = Time.time;
-                numberOfClicks++;
-
-                if (numberOfClicks == 1)
-                {
-                    anim.SetBool("Attack1", true);
-                }
-                numberOfClicks = Mathf.Clamp(numberOfClicks, 0, 2);
-
-                if (numberOfClicks >= 1 && anim.GetCurrentAnimatorStateInfo(2).normalizedTime > .2f && anim.GetCurrentAnimatorStateInfo(2).IsName("Attack1"))
-                {
-                    playNext = true;
-
-                }
-
-                if (numberOfClicks >= 2 && anim.GetCurrentAnimatorStateInfo(2).normalizedTime > .2f && anim.GetCurrentAnimatorStateInfo(2).IsName("Attack2"))
-                {
-                    playNext = true;
-
-                }
-            }
-            else if(hasTool)
-            {
-                anim.SetBool("hasSword", false);
-                anim.SetBool("isTool", true);
-                anim.SetTrigger("mine");
-            }
-
-            anim.SetLayerWeight(2, 1);
-        }
-
-        private void ResetAnimationBools()
-        {
-            if (anim.GetCurrentAnimatorStateInfo(2).normalizedTime > .9f && anim.GetCurrentAnimatorStateInfo(2).IsName("Attack1"))
-            {
-                anim.SetBool("Attack1", false);
-            }
-            if (anim.GetCurrentAnimatorStateInfo(2).normalizedTime > .9f && anim.GetCurrentAnimatorStateInfo(2).IsName("Attack2"))
-            {
-                anim.SetBool("Attack2", false);
-                anim.SetLayerWeight(2, 0);
-                numberOfClicks = 0;
-            }
-            if (anim.GetCurrentAnimatorStateInfo(2).normalizedTime > .9f && anim.GetCurrentAnimatorStateInfo(2).IsName("Attack3"))
-            {
-                anim.SetBool("Attack3", false);
-                anim.SetLayerWeight(2, 0);
-                numberOfClicks = 0;
-            }
-
-            if (Time.time - lastTimeClicked > maxComboDelay)
-            {
-                numberOfClicks = 0;
-            }
-        }
     }
 }
